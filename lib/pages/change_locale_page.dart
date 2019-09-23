a0521daf5320c51b2285bc44bcdf03b2dc586c09
刘乃彬 <nbliu@91jkys.com>
2019-09-23 14:32:37 +0800

    feat($features): 主题切换done，多语言未处理

--- lib/pages/change_locale_page.dart
+++ lib/pages/change_locale_page.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_github_client_app/states/profile_data.dart';
+import 'package:provider/provider.dart';
 
 class ChangeLocalePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var color = Theme.of(context).primaryColor;
+    var localeModel = Provider.of<AppLocaleData>(context);
+    //var gm = GmLocalizations.of(context);
+    //构建语言选择项
+    Widget _buildLanguageItem(String lan, value) {
+      return ListTile(
+        title: Text(
+          lan,
+          // 对APP当前语言进行高亮显示
+          style: TextStyle(color: localeModel.locale == value ? color : null),
+        ),
+        trailing:
+            localeModel.locale == value ? Icon(Icons.done, color: color) : null,
+        onTap: () {
+          // 更新locale后MaterialApp会重新build
+          localeModel.locale = value;
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('语言设置'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _buildLanguageItem("中文简体", "zh_CN"),
+          _buildLanguageItem("English", "en_US"),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/change_theme_page.dart
+++ lib/pages/change_theme_page.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_github_client_app/common/global.dart';
+import 'package:flutter_github_client_app/states/profile_data.dart';
+import 'package:provider/provider.dart';
 
 class ChangeThemePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('主题选择'),
+      ),
+      body: ListView(
+        //显示主题色块
+        children: Global.themes.map<Widget>((e) {
+          return GestureDetector(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+              child: Container(
+                color: e,
+                height: 40,
+              ),
+            ),
+            onTap: () {
+              //主题更新后，MaterialApp会重新build
+              Provider.of<AppThemeData>(context).theme = e;
+            },
+          );
+        }).toList(),
+      ),
+    );
   }
 }
